d721817e19003d0eaeadb4807d3fa13e201a6227
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-15 10:44:46 +0100

    Update SignEvents.php

--- src/EssentialsPE/EventHandlers/SignEvents.php
+++ src/EssentialsPE/EventHandlers/SignEvents.php
@@ -161,12 +161,12 @@ class SignEvents extends BaseEventHandler{
                         }
                         foreach ($event->getPlayer()->getInventory()->getContents(true) as $item){
                             if($this->getAPI()->isRepairable($item)){
-                                $event->getInventory()->setItem($item)->setDamage(0);
+                                $event->getPlayer()->getInventory()->setDamage(0);
                             }
                         }
-                        foreach ($event->getPlayer()->getArmorInventory()->getContents(true) as $armor){
+                        foreach ($event->getPlayer()->getArmorInventory()->getContents(true) as $item){
                             if($this->getAPI()->isRepairable($item)){
-                                $event->getArmorInventory()->setItem($item)->setDamage(0);
+                                $event->getPlayer()->getArmorInventory()->setDamage(0);
                             }
                         }
                         $event->getPlayer()->sendMessage(TextFormat::GREEN . "All the tools on your inventory were repaired" . TextFormat::AQUA . "\n(including the equipped Armor)" . TextFormat::GREEN . ($price ? " for " . $this->getAPI()->getCurrencySymbol() . $price : null));
